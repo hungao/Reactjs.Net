399a611a373f5f672bdeee98327f42f3ed3dd05a
aohoanghung <aohoanghung@VNDTHRSVR01>
2018-08-09 15:24:20 +0700

    Add project files.

---
+++ WebApplication3/App_Start/BundleConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.Web;
+using System.Web.Optimization;
+using System.Web.Optimization.React;
+
+namespace WebApplication3
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new BabelBundle("~/bundles/main").Include(
+                    "~/Scripts/remarkable.min.js",
+                    "~/Scripts/Tutorial.jsx"
+                ));
+        }
+    }
+}
